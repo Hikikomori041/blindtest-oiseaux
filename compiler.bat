b1ac9826ef29481be6758b52af5dfb239e7fb8b7
BLACHERE Nicolas <nicolas.blachere@gmail.com>
2025-04-07 22:50:49 +0200

    v1.4 - Ajout de 3 oiseaux de plaine

--- compiler.bat
+++ compiler.bat
@@ -1,12 +1,12 @@
 @echo off
 echo Compilation en cours...
 python -m PyInstaller --noconfirm --onefile --windowed ^
---name "Blind-Test Oiseaux Charente" ^
+--name "Blind-Test Oiseaux" ^
 --icon=ressources/images/oiseau.ico ^
 --add-data "ressources;ressources" ^
 Blind-Test.py > dist/compil.log 2>&1
 
-IF EXIST dist\\Blind-Test.exe (
+IF EXIST "dist\\Blind-Test Oiseaux.exe" (
     echo Compilation reussie !
 ) ELSE (
     echo Erreur pendant la compilation. Verifier les chemins et fichiers...
